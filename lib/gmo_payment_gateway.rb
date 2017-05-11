6db5c7235b36c914a939c0926285d8b72430da52
naofumi-fujii <naofumi.fujii@gmail.com>
2017-05-11 22:57:23 +0900

    search_trade_multi

--- lib/gmo_payment_gateway.rb
+++ lib/gmo_payment_gateway.rb
@@ -1,5 +1,25 @@
 require "gmo_payment_gateway/version"
+require "faraday"
 
 module GmoPaymentGateway
-  # Your code goes here...
+  class Client
+    attr_reader *%i(faraday shop_id shop_pass)
+
+    def initialize(url, shop_id, shop_pass)
+      @faraday   = Faraday.new(url: url)
+      @shop_id   = shop_id
+      @shop_pass = shop_pass
+    end
+
+    def search_trade_multi(order_id, pay_type)
+      response = faraday.post '/payment/SearchTradeMulti.idPass', {
+        ShopID:   shop_id,
+        ShopPass: shop_pass,
+        OrderID:  order_id,
+        PayType:  pay_type,
+      }
+
+      CGI.parse(response.body).map {|k, v| {k => v[0]}}.inject(&:merge)
+    end
+  end
 end
